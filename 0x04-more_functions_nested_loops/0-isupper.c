ccb092c5d3b64bc41d1da6ae542f2e9b8c461120
johnodaiobodai <johnodaiobodai@gmail.com>
2022-07-12 03:31:38 +0200

    nested

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _isupper - A function that checks for uppercase character.
  * @c: An input character
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,13 +7,13 @@
 
 int _isdigit(int c)
 {
-if (c >= 48 && c <= 57)
-{
-return (1);
-}
+    if (c >= 48 && c <= 57)
+    {
+        return (1);
+    }
 
-else
-{
-return (0);
-}
+    else
+    {
+        return (0);
+    }
 }
